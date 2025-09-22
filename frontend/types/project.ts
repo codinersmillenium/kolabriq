d3c265fad64b385f998ecac6a00bae515f5c085a
codinersmillenium <>
2025-09-23 07:20:03 +0800

    deploy update landing page

--- frontend/types/project.ts
+++
@@ -1,3 +0,0 @@
-export type TeamKey = "frontend" | "backend" | "ui" | "bussines_analist";
-export type StatusType = "new" | "in_progress" | "review" | "done";
-export type ProjectType = "free" | "rewarded";
